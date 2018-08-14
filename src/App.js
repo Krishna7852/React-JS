b2e6f52e43531fedb6d3d2e474e93242852e3044
krishna <bhamare.k@greytip.com>
2018-08-14 12:53:53 +0530

    added lifecycle hook for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import {HomeComponent} from "./home-index";
 class App extends Component {
   render() {
--- src/home/home.js
+++ src/home/home.js
@@ -1,8 +1,33 @@
 import React, {Component} from 'react';
 import {TopBarComponent} from "../home-index";
-export class HomeComponent extends Component{
+
+export class HomeComponent extends Component {
+    constructor(props) {
+        console.log('constructor called-->', props);
+        super(props);
+        console.log('constructor && supper-->' , props);
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate called-->');
+    }
+    componentWillUpdate() {
+        console.log('componentWillUpdate called-->');
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate called-->', nextProps, nextState);
+    }
+    componentWillReceiveProps() {
+        console.log('componentWillReceiveProps called-->');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount called-->');
+    }
+    componentDidMount() {
+        console.log('componentDidMount called-->');
+    }
     render() {
-        return(
+        return (
             <div>
                 <TopBarComponent/>
             </div>
